a42ae4a40e9c56c8e0cb32036254c0b9383402de
summerdixon <summerdixon2023@g.ucla.edu>
2024-11-11 22:44:07 -0800

    Placeholder for scrolling feature and new branch.

--- tastebuds-site/src/App.js
+++ tastebuds-site/src/App.js
@@ -1,10 +1,11 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Login from './components/Login';  // If Login.js is in the components folder
-import Signup from './pages/signup'; 
-import HomePage from './pages/home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';  // Ensure this path is correct
+import Signup from './pages/signup'; // Correct path for signup page
+import HomePage from './pages/home';  // Correct path for HomePage
+import ScrollPage from './pages/scroll'; // Correct path for ScrollPage
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <div>
         {/* Define Routes here */}
         <Routes>
-        <Route path = "/" element = {<HomePage />} />
-        <Route path="/signup" element={<Signup />} /> {/* signup page route */}
-        <Route path="/Login" element={<Login />} /> {/* signup page route */}
+          <Route path="/" element={<HomePage />} /> {/* Home page route */}
+          <Route path="/signup" element={<Signup />} /> {/* Signup page route */}
+          <Route path="/login" element={<Login />} /> {/* Login page route */}
+          <Route path="/scroll" element={<ScrollPage />} /> {/* Scroll page route */}
         </Routes>
       </div>
     </Router>
