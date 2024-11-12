1970bb41242d71fdfffd1a85c498b4f829484d9e
Anthony Zhao <anthonyzhao23@gmail.com>
2024-11-11 23:51:49 -0800

    Updated .json filles and App.js

--- tastebuds-site/src/App.js
+++ tastebuds-site/src/App.js
@@ -1,13 +1,14 @@
 // src/App.js
 import React from 'react';
-import './App.css';
+// import './App.css';
 import Login from './components/Login';  // If Login.js is in the components folder
+import SwipePage from './page/SwipePage';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to TasteBuds</h1>
-      <Login /> {/* Render the Login component */}
+      <SwipePage /> 
     </div>
   );
 }
