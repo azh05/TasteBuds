444d5a398f49942a06062adf835fe7e55044f4dc
brandonngu64 <fishadchips798@gmail.com>
2024-11-24 20:17:08 -0800

    Added react-google-maps import and API Key. Added sample map display.

--- tastebuds-site/src/pages/restaurant.js
+++ tastebuds-site/src/pages/restaurant.js
@@ -1,17 +1,45 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import React, { useState, useRef } from "react";
+import { StandaloneSearchBox } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import '../App.css'; // Import CSS file for styling
 
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat: 7.2905715, // default latitude
+  lng: 80.6337262, // default longitude
+};
+
+
 function Restaurant() {
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyAGFYZ_ymrTuNW3Xqy48RASAK6MN8jouKA',
+        libraries,
+      });
+    
+      if (loadError) {
+        return <div>Error loading maps</div>;
+      }
+    
+      if (!isLoaded) {
+        return <div>Loading maps</div>;
+      }
+    
     return (
-        <div className="match-page-total">
-            <h1 className="match-title" >Matches</h1>
-        <div className="horizontal-line" ></div>
-        <div className = "match-container">
-        </div>
-        </div>
-        
-    )
-}
+        <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </div>
+  )
+};
 
 export default Restaurant;
