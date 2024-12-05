2a9e39189b65a29e6f35e3c500aae243c28c6f65
brandonngu64 <fishadchips798@gmail.com>
2024-12-04 17:23:07 -0800

    Added code to fetch user profile from backend, added code to fetch user's favorite cuisine.

--- tastebuds-site/src/pages/recomend.js
+++ tastebuds-site/src/pages/recomend.js
@@ -6,38 +6,48 @@ import { Link } from 'react-router-dom';
 
 function Recomend() {
     const { user } = useUser();
-    const [profiles, setProfiles] = useState([]);
-    const [allProfiles, setAllProfiles] = useState([]);
-      // Fetch all user profiles when the component mounts
-useEffect(() => {
-    fetch("http://localhost:5001/all_users") // Ensure this matches your backend URL
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Server error: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((users) => {
-        if (Array.isArray(users)) {
-          // Filter out the current user's profile
-          const filteredUsers = users.filter((profile) => profile.email !== user.email);
-  
-          setAllProfiles(filteredUsers); // Store all other profiles
-          setProfiles(filteredUsers); // Display all other profiles initially
-        } else {
-          console.error("Invalid data format:", users);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching profiles:", error);
+    const [favoriteCusine, setFavoriteCusine] = useState(null);
+    const [userMatch, setUserMarch] = useState(null);
+    const [recomendation, setRecomendation] = useState(null);
+    const [profileData, setProfileData] = useState({
+        profileName: '',
+        age: '',
+        gender: '',
+        cuisine: [],
+        bio: '',
+        icon: '',
       });
-  }, [user.email]);
+      useEffect(() => {
+        const allTags = ["Italian", "Indian", "Japanese", "American", "Other"];
+        // Fetch profile data
+        const fetchProfile = async () => {
+          try {
+            const email = user.email;
+            const response = await fetch(`http://localhost:5001/profile?email=${email}`);
+            console.log("fetched user from backend API")
+            if (!response.ok) {
+              throw new Error('Failed to fetch profile');
+            }
+            const data = await response.json();  // Parse the response as JSON
+            setProfileData((prev) => ({
+              ...data,
+            }));
+        
+    
+          } catch (error) {
+            console.error('Error fetching profile data:', error);
+          }
+        }
+        if (user) {
+          fetchProfile();
+        }}, [user]);
 
-  
-
+    
     return (
         <div>
             <Navbar></Navbar>
+            <div>Since you like {profileData.cuisine[0]} we recomend...
+            </div>
         </div>
     );
 };
